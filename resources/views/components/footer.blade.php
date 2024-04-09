ee7d2d0b52be769fbd7038371f8c926b23e9002a
LuciaSilla <luciasilla@outlook.it>
2024-04-09 21:15:35 +0200

    aggiunto footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,13 +1,13 @@
 
-
+<footer class="bg-body-tertiary mt-5">
     <div class="container justify-content-center">
         <div class="row">
-            <div class="col-12">
-                <p>Lavora con noi</p>
-                <p>Registrati</p>
+            <div class="col-12 text-center">
+                <p class="mt-5">Lavora con noi</p>
+                <a class="nav-link" href="/register">Registrati</a>
                 <p>clicca qui</p>
                 <a href="{{route('become.revisor')}}">Diventa revisore</a>
             </div>
         </div>
     </div>
-
+</footer>
