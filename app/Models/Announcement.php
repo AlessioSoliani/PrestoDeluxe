7a8975d0951046c5a66b4c11bcc6dd5ad96334de
Alessio Soliani <alessiosoliani2gmail.com>
2024-04-09 19:27:47 +0200

    modifiche generali

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -23,7 +23,7 @@ class Announcement extends Model
     public function setAccepted($value){
         //il campo "is_accepted" dell'annuncio sul quale e richiamato il metodo sarà uguale al valore datogli dal revisore
         // true se accetta false se rifiuta(il parametro che arriverà)
-        $this->is_accepted =$value;
+        $this->is_accepted = $value;
         //salviamo la modifica
         $this->save();
         
