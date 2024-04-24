65c6b0dc166271da711b6409071c3c3dd156dbe1
Beatrice Carattoli <carattolibea@gmail.com>
2024-04-24 21:24:44 +0200

    piccole modifiche frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
     <div class="hero">
         <video controls autoplay muted loop id="video-homepage">
-          <source src="video-homepage.mp4" type="video/mp4">
+          <source src="video-hero.mp4" type="video/mp4">
         </video>
         <div class="hero_content">
           <h1>{{__('ui.titlesite')}}</h1>
@@ -17,7 +17,7 @@
     <section class="container">
         <div class="row justify-content-center">            
             @foreach ( $announcements as $announcement )
-              <div class=" card-style-home col-12 col-md-4 card " style="width: 18rem;">
+              <div class="card-style-home col-12 col-md-4 rounded" style="width: 18rem;">
                   <div class=" d-flex justify-content-center">
                        <img class="mt-3 img-style" src="{{!$announcement->images()->get()->isEmpty() ? $announcement->images()->first()->getUrl(500,500) : 'http://picsum.photos/200'}}" >
                   </div>
@@ -26,13 +26,13 @@
                   </div>
                   <ul class="list-group list-group-flush">
                       {{-- <li class="text-center styleColor list-group-item">{{$announcement->body}}</li> --}}
-                      <li class="text-center styleColor list-group-item">{{$announcement->price}}€</li>
+                      <li class="text-center bg-transparent list-group-item">{{$announcement->price}}€</li>
                       {{-- <li class="list-group-item">{{$announcement->category}}$</li> --}}
                       {{-- <li class="text-center styleColor list-group-item "> {{__('ui.date')}}:{{$announcement->created_at->format('d/m/Y')}}</li> --}}
                   </ul>
                   <div class=" d-flex justify-content-around card-body my-3">
-                        <a class="btn btn-outline-light lenguages" href="{{route('announcements.show', compact('announcement'))}}">{{__('ui.show')}}</a>
-                        <a class="btn btn-outline-light lenguages" href="{{route('categoryShow', ['category'=>$announcement->category])}}">{{__('ui.'.$announcement->category->name)}}</a>
+                        <a class="btn btn-outline-light lenguages rounded" href="{{route('announcements.show', compact('announcement'))}}">{{__('ui.show')}}</a>
+                        <a class="btn btn-outline-light lenguages rounded" href="{{route('categoryShow', ['category'=>$announcement->category])}}">{{__('ui.'.$announcement->category->name)}}</a>
                   </div>
               </div>
             @endforeach()               
