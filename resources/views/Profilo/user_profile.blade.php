0abdac55871a6950424fb7a5901678a1efdefcdf
Alessio Soliani <alessiosoliani2gmail.com>
2024-04-30 23:55:44 +0200

    merge

--- resources/views/Profilo/user_profile.blade.php
+++ resources/views/Profilo/user_profile.blade.php
@@ -68,7 +68,7 @@
                         <li class="text-center bg-transparent text-white list-group-item">{{$announcement->price}}€</li>
                         
                     </ul>
-                    {{-- <div class=" d-flex justify-content-around card-body my-3">
+                    <div class=" d-flex justify-content-around card-body my-3">
                           <a class="btn btn-outline-light rounded" href="{{route('announcements.show', compact('announcement'))}}">{{__('ui.show')}}</a>
                           <a class="btn btn-outline-light rounded" href="{{route('categoryShow', ['category'=>$announcement->category])}}">{{__('ui.'.$announcement->category->name)}}</a>
                           <form method="POST" action="{{route('destroy_Ad',$announcement->id)}}">
@@ -77,81 +77,14 @@
                             <button type="submite" class="btn btn-outline-danger">delete</button>
                         </form>
                     </div>
-                </div> --}}
-                <div class=" d-flex justify-content-around card-body my-3">
-                    <a class="btn btn-outline-light rounded" href="{{route('announcements.show', compact('announcement'))}}">{{__('ui.show')}}</a>
-                    <a class="btn btn-outline-light rounded" href="{{route('categoryShow', ['category'=>$announcement->category])}}">{{__('ui.'.$announcement->category->name)}}</a>
-                    <form method="POST" action="{{route('destroy_Ad',$announcement->id)}}">
-                      @csrf
-                      @method('DELETE')
-                      <button type="submite" class="btn btn-outline-danger">delete</button>
-                  </form>
-              </div>
-            </div>
+                </div>
                 @endforeach
             
             @endif
         </div>
     </section>
 
-    {{-- <section id="modifica-profilo" class="mt-5">
-        <div class="deluxe">  
-            <p class='text-center'>modifica il tuo profilo</p>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-12 col-md-4">
-                        <form method="POST" action="/user/profile-information" enctype="multipart/form-data" >
-                            @csrf
-                            @method('PUT')
-                            <div class="mb-3">
-                                <label class="form-label">{{__('ui.Name')}}</label>
-                                <input type="text" class="form-control" name="name" value="{{old('name')}}">
-                                @error('name')
-                                  <span>{{$message}}</span>
-                                @enderror
-                            </div>                      
-                               <div class="mb-3">
-                                 <label class="form-label"> {{__('ui.AccountEmail')}}</label>
-                                 <input type="email" class="form-control" name="email"  value="{{old('email')}}">
-                                  @error('email')
-                                   <span>{{$message}}</span>
-                                  @enderror
-                               </div>  
-                               <div>
-                                <label class="form-label" for="user_photo">User Photo</label>
-                                <input id="user_photo" type="file" class="form-control" name="user_photo" value="{{ old('user_photo') }}" required autofocus>
-                            </div>
-                            
-                            <div>
-                                <label class="form-label" for="telephone_number">Telephone Number</label>
-                                <input id="telephone_number" type="tel" class="form-control" name="telephone_number" value="{{ old('telephone_number') }}" required>
-                            </div>
-                            
-                            <div>
-                                <label class="form-label" for="location">Location</label>
-                                <input id="location" type="text" class="form-control" name="location" value="{{ old('location') }}" required>
-                            </div>                    
-                               <div class="mb-3">
-                                 <label class="form-label">{{__('ui.Password')}}</label>
-                                 <input type="password" class="form-control" name="password">
-                                 @error('password')
-                                   <span>{{$message}}</span>
-                                 @enderror
-                               </div>                     
-                              <div class="mb-3">
-                                <label class="form-label">{{__('ui.ConfirmPassword')}} </label>
-                                <input type="password" class="form-control" name="password_confirmation">
-                             </div>
-                             <div class="d-flex justify-content-center">
-                              <button type="submit" class="btn btn-outline-light lenguages">{{__('ui.Send')}}</button>
-                            </div>
-                         </form>
-                    </div>
-                </div>
-            </div>
-          </div>  
-    </section> --}}
-
+    
 </x-main>
 
 
